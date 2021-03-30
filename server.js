afe0ec7dcb2a5faa64f5f564cfe9a3b387fb2fd8
Clayton Miller <clayton.miller@loop.colum.edu>
2021-03-30 15:50:48 -0600

    Changes Port to process.env or go to local port 3000

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
+require("dotenv").config()
 const express = require('express')
 const app = express()
-const {PORT = 3000, NODE_ENV = "development" } = process.env
-const logger = require("morgan");
-const cors = require("cors");
-const corsOptions = require("./configs/cors.js");
+const PORT = process.env.PORT || 3000
+const logger = require("morgan")
+const cors = require("cors")
+const corsOptions = require("./configs/cors.js")
 
 const studentController = require("./controllers/studentController")
 
 app.use(express.urlencoded({extended:false}))
-NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors())
 app.use(express.json())
-app.use(logger("dev"));
+app.use(logger("dev"))
 
 app.get("/", (req,res) => {
     res.json({
