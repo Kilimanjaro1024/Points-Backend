4124a5307761c20dfac6f37972011e6e872e11db
Clayton Miller <clayton.miller@loop.colum.edu>
2021-03-30 16:39:08 -0600

    reimplements .env encoding

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const DB = mongoose.connection
 const { MONGODBURI } = process.env
 const config = { useUnifiedTopology: true, useNeUrlParser: true }
 
-mongoose.connect("mongodb+srv://1207:1207@cluster0.g8p4x.mongodb.net/Test?retryWrites=true&w=majority" , config)
+mongoose.connect(MONGODBURI , config)
 
 DB.on("open", () => console.log("You are connected to Mongo"))
     .on("close", () => console.log("You are disconnected from Mongo"))
