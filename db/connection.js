e82754f9409e6ab0722d9af5ce62e56ac3726dd9
Clayton Miller <clayton.miller@loop.colum.edu>
2021-03-30 16:21:29 -0600

    hardcodes in connection string for mongoDB

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 require("dotenv").config()
 const mongoose = require("mongoose")
 const DB = mongoose.connection
-const config = { useUnifiedTopology: true, useNeUrlParser: true }
 const { MONGODBURI } = process.env
+const config = { useUnifiedTopology: true, useNeUrlParser: true }
 
-mongoose.connect(MONGODBURI , config)
+mongoose.connect("mongodb+srv://1207:1207@cluster0.g8p4x.mongodb.net/Test?retryWrites=true&w=majority" , config)
 
 DB.on("open", () => console.log("You are connected to Mongo"))
     .on("close", () => console.log("You are disconnected from Mongo"))
